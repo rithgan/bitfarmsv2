92ee95cdee29c77fc8886c782018565213b12570
rithgan <rithikgandhi1989@gmail.com>
2022-01-20 01:12:36 +0530

    stake

---
+++ src/components/stake/Stake.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PrimaryContainer from "../primaryContainer/PrimaryContainer";
+
+export default function Stake() {
+	return (
+		<div>
+			<PrimaryContainer />
+		</div>
+	);
+}
